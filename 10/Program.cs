06169d9dfa01188256ca3b23a3bd6b2a06ab6e2f
Opex <prokud1995@gmail.com>
2022-12-20 02:07:06 +0500

    Домашнее задание 3

---
+++ 10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите N: ");
+int N = int.Parse (Console.ReadLine());
+for (int I = 1; I<=N; I++)
+{
+    if (I==N){Console.Write($"{Math.Pow(I, 3)}");}
+    else Console.Write($"{Math.Pow(I, 3)}, ");
+}
---
+++ 8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int x = int.Parse (Console.ReadLine());
+int a = x/10000;
+int b = (x/1000)%10;
+int c = (x/10)%10;
+int d = x%10;
+if (a==d)
+{
+    if (b==c)
+    {
+        Console.Write($"Ваше число является палиндромом!");
+    }
+    else
+    {
+        Console.Write($"Ваше число не является палиндромом!");
+    }
+}
+else 
+{
+    Console.Write($"Ваше число не является палиндромом!");
+}
+    
