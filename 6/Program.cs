bdc4cc1ff240058cf21aabb755e42a562eee4b65
Opex <prokud1995@gmail.com>
2022-12-20 03:06:40 +0500

    Домашнее задание 2.

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.WriteLine("Введите число: ");
 int x = int.Parse (Console.ReadLine());
 int index = 2;
+if (x/100 > 0)
+{
 int number = x.ToString()[index] - '0'; //Честно, гуглил эту строку, такое же не возброняется?:)
-Console.Write($"{number}");
-
-
-
-
-
+Console.Write($"Третья цифра вашего числа: {number}");
+}
+else
+{
+    Console.Write($"Ошибка!");
+}
 
 
 
