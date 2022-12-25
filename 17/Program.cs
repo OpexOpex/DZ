097b79f911fb876e4ac3be1ae1b0d000c7e16e13
Opex <prokud1995@gmail.com>
2022-12-25 23:38:11 +0500

    дз 5

---
+++ 17/Program.cs
@@ -0,0 +1,50 @@
+﻿// Сколько чисел больше 0
+
+int length = ReadInt("Array length");
+int min = ReadInt("Min value");
+int max = ReadInt("Max value");
+int[] array = GetRandomArray(length, min, max);
+
+PrintArray(array);
+Console.WriteLine();
+Positive(array);
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+	int[] array = new int[length];
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(minValue, maxValue + 1);
+	}
+
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
+
+void Positive(int[] array)
+{
+    int count=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (array[i]>0)
+      {
+        count=count+1;
+      }
+    }
+    System.Console.WriteLine($"Количество положительных чисел в заданном массиве = {count}");
+}
---
+++ 18/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2
+
+Console.WriteLine("Введите число k1: ");
+double k1 = double.Parse (Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+double b1 = double.Parse (Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+double k2 = double.Parse (Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = double.Parse (Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+
+System.Console.WriteLine($"Координаты точки пересечения прямых ({x}; {y})");
