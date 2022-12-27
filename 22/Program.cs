b84cb48810d511700e12b1a8c53db6c5616dbfc2
Opex <prokud1995@gmail.com>
2022-12-27 05:26:53 +0500

    дз

---
+++ 22/Program.cs
@@ -0,0 +1,58 @@
+﻿// Среднее арифметическое элементов в каждом столбце двумерного массива.
+
+System.Console.WriteLine("Введите длину массива:");
+int length = ReadInt();
+System.Console.WriteLine("Введите ширину массива:");
+int secondLength = ReadInt();
+int[,] array = GetArray(length, secondLength);
+PrintArray(array);
+Console.ReadLine();
+Average(array);
+double a;
+
+int ReadInt() 
+{
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GetArray (int length, int secondLength)
+{
+    int[,] array = new int[length, secondLength]; 
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = random.Next(10); 
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Average(int[,] array)
+{
+    double sum = 0;
+    double a = 0;
+    for (int col = 0; col < secondLength; col++ )
+    {
+        sum = 0; 
+       for (int row = 0; row < length; row++ )
+       {
+        sum += array[row, col];
+       }
+       a = (sum/length);
+       Console.Write($"{a} ");
+    }
+}
