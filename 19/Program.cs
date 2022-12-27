bbfe91ffa9173d894692eb1c3beb4c06cba48833
Opex <prokud1995@gmail.com>
2022-12-27 05:28:43 +0500

    дз

---
+++ 19/Program.cs
@@ -0,0 +1,39 @@
+﻿// Двумерный массив вещественных чисел
+System.Console.WriteLine("Введите размер M");
+int length = ReadInt();
+System.Console.WriteLine("Введите размер N");
+int secondLength = ReadInt();
+double[,] array = GetArray(length, secondLength);
+PrintArray(array);
+Console.ReadLine();
+
+int ReadInt() 
+{
+    return int.Parse(Console.ReadLine());
+}
+
+double[,] GetArray (int length, int secondLength)
+{
+    double[,] array = new double[length, secondLength]; 
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = 1 + random.NextDouble()*99;
+        }
+    }
+    return array;
+}
+
+void PrintArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 20/Program.cs
@@ -0,0 +1,46 @@
+﻿// Поиск элемента по позиции
+
+System.Console.WriteLine("Введите длину массива:");
+int length = ReadInt();
+System.Console.WriteLine("Введите ширину массива:");
+int secondLength = ReadInt();
+int[,] array = GetArray(length, secondLength);
+PrintArray(array);
+System.Console.WriteLine("Введите позицию элемента по длине:");
+int i = ReadInt();
+System.Console.WriteLine("Введите позицию элемента по ширине:");
+int j = ReadInt();
+
+if (i<=length && j<=secondLength) Console.WriteLine($"Запрошенный элемент: {array[i-1, j-1]}");
+else Console.WriteLine("Такого числа в массиве нет");
+
+int ReadInt() 
+{
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GetArray (int length, int secondLength)
+{
+    int[,] array = new int[length, secondLength]; 
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = random.Next(1000); 
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
