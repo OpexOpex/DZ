29101ebb6244086152b483c6ccc6b67987c8b861
Opex <prokud1995@gmail.com>
2022-12-31 14:23:46 +0500

    дз?

---
+++ 22/Program.cs
@@ -0,0 +1,73 @@
+﻿// упорядочить по убыванию элементы каждой строчки массива
+// каунтченджес написал на всякий случай, в интернете прочитал, что лучше с ним, чтобы не гонять много раз по изначально упорядроченной стрке
+
+System.Console.WriteLine("Введите длину массива:");
+int length = ReadInt();
+System.Console.WriteLine("Введите ширину массива:");
+int secondLength = ReadInt();
+int[,] array = GetArray(length, secondLength);
+PrintArray(array);
+Console.ReadLine();
+ChangePosition(array);
+
+int ReadInt() 
+{
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GetArray (int length, int secondLength)
+{
+    int[,] array = new int[length, secondLength]; 
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = random.Next(10); 
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangePosition(int[,] array)
+{
+    int temp=0;
+    for (int row = 0; row < length; row++ )
+    {
+
+       for (int i = 0; i < secondLength-1; i++ )
+       {
+            // int countChanges=0;
+            for (int col = 0; col < secondLength; col++ )
+            {
+                if(col<secondLength-1)
+                {
+                    if (array[row, col]<array[row, col+1])
+                    {
+                        temp = array[row, col];
+                        array[row, col] = array[row, col+1];
+                        array[row, col+1] = temp;
+                        // countChanges++;
+                    }
+                }
+            }
+            // if(countChanges == 0)
+            // {
+            //     break;
+            // }
+       }
+    }
+    PrintArray(array);
+}
---
+++ 23/Program.cs
@@ -0,0 +1,61 @@
+﻿// Найти строку с наименьшей суммой элементов
+
+System.Console.WriteLine("Введите длину массива:");
+int length = ReadInt();
+System.Console.WriteLine("Введите ширину массива:");
+int secondLength = ReadInt();
+int[,] array = GetArray(length, secondLength);
+PrintArray(array);
+Console.ReadLine();
+GetSum(array);
+
+int ReadInt() 
+{
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GetArray (int length, int secondLength)
+{
+    int[,] array = new int[length, secondLength]; 
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = random.Next(10); 
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetSum(int[,] array)
+{
+    int minRow=0;
+    int min=int.MaxValue;
+    for (int row = 0; row < length; row++ )
+    {
+        int sum=0;
+        for (int col = 0; col < secondLength; col++ )
+        {
+            sum=sum+array[row, col];
+        }
+        if (sum<min)
+        {
+            min=sum;
+            minRow=row+1;
+        }
+    }
+    System.Console.WriteLine($"Номер строки с минимальной суммой: {minRow}");
+}
